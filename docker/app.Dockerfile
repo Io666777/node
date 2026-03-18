81417946f409d38c1c9a82e09bf1fa64c4b0b8d2
Io666777 <dimaskitihin@gmail.com>
2026-03-18 16:39:31 +0300

    feat/done

--- docker/app.Dockerfile
+++ docker/app.Dockerfile
@@ -1,11 +1,14 @@
 FROM node:22-alpine
 
-
 WORKDIR /usr/app
 
 COPY package*.json ./
 
+COPY prisma ./prisma/
+
 RUN npm install
+
+RUN npx prisma generate
 
 COPY . .
 
--- src/prisma.ts
+++ src/prisma.ts
@@ -1,13 +1,10 @@
 import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 
-const prisma = new PrismaClient({
-  // @ts-ignore - игнорируем проверку типов для этого костыля
-  __internal: {
-    engine: {
-      endpoint: undefined,
-      type: 'library' // Явно заставляем использовать библиотечный движок
-    }
-  }
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
 });
+
+const prisma = new PrismaClient({ adapter });
 
 export default prisma;
