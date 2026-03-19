57b23261fc3823a7488e5fd4a5aee407b2c02a04
Io666777 <dimaskitihin@gmail.com>
2026-03-19 11:17:41 +0300

    wip/seed: ошибки с докером

---
+++ prisma/seed.ts
@@ -0,0 +1,38 @@
+import { PrismaClient, Prisma } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
+// import prisma from '../src/prisma'
+
+const prisma = new PrismaClient();
+
+async function main() {
+const hashedPassword = await bcrypt.hash('admin', 10);
+const adminData: Prisma.AdminCreateInput[] = [
+    {
+      name: 'LEGOO',
+      login: 'LEGOO',
+      password: hashedPassword,
+    },
+    {
+      name: 'Administrator',
+      login: 'admin',
+      password: hashedPassword,
+    },
+];
+for (const ad of adminData) {
+    await prisma.admin.upsert({
+      where: { login: ad.login },
+      update: {},
+      create: ad,
+    });
+  }
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error('Ошибка при сидировании:', e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
